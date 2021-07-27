84299ec022b37c056b922dbbd9fd5967cfe8634c
gadrian99 <adriangarcia1999@yahoo.com>
2021-07-27 01:23:40 -0400

    ropsten deployment config

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,9 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
-const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 const privateKeys = process.env.PRIVATE_KEYS || ""
+const MNEMONIC = 'rent gesture robust nut perfect bird risk planet scout usage cup mobile'
 
 module.exports = {
   networks: {
@@ -13,14 +14,13 @@ module.exports = {
     },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(
-          privateKeys.split(','), // Array of account private keys
-          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
-        )
+        return new HDWalletProvider(MNEMONIC, "https://mainnet.infura.io/v3/ea6a8a821e5144d4b5e77f7aed15db03")
+          // privateKeys.split(','), // Array of account private keys
+          // `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
       },
-      gas: 5000000,
+      gas: 4000000,
       gasPrice: 25000000000,
-      network_id: 3
+      network_id: 1
     }
   },
   contracts_directory: './src/contracts/',
