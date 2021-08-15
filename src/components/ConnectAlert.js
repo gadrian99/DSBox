599eb7cb8d93402bdb019ed55fc49dc7a9440463
gadrian99 <adriangarcia1999@yahoo.com>
2021-08-15 11:49:27 -0400

    navbar rework

--- src/components/ConnectAlert.js
+++ src/components/ConnectAlert.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { RefreshCcw, AlertOctagon } from 'react-feather'
+import { useSpring, animated } from 'react-spring'
 
 function ConnectAlert() {
+    const spring = useSpring({ to: { opacity: 1 }, from: { opacity: 0 }, delay: 200 })
     return(
-        <div className="connect-alert">
+        <animated.div style={spring} className="connect-alert">
             <AlertOctagon size={'7rem'} color={'#CF6679'} />
             <p className="connect-alert-primary">Oops, it looks like there was an error...</p>
-            <p className="connect-alert-secondary">Please make sure Ganache is running and your Metamask wallet is connected to the right network.</p>
+            <p className="connect-alert-secondary">Please make sure your Metamask wallet is connected to the right network.</p>
             <button className="connect-alert-button" onClick={() => window.location.reload()}><RefreshCcw style={{ marginRight: '1rem' }}/>Reload page</button>
-        </div>
+        </animated.div>
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Folder, Film, Image, CornerRightUp } from 'react-feather';
+import { Folder, Film, Image, CornerRightUp, Settings } from 'react-feather';
 import {
   Link
 } from "react-router-dom";
@@ -9,39 +9,8 @@ class Navbar extends Component {
     return (
       <div className="navigation">
         <img alt="logo" src="/assets/Logo.svg" style={{ height: 'auto', width: '3.5rem'}} className="logo"></img>
-          <ul className="navigation-icons">
-            {/* <Link to="/">
-              <li className={this.props.page === "home" ? "active" : " "} onClick={(e) => this.props.setPage("home")} >
-                <Home className="nav-icon"/>
-                <p className="nav-text">Home</p>
-              </li>
-            </Link> */}
-            <Link to="/">
-              <li className={this.props.page === "all" ? "active" : " "} onClick={(e) => this.props.setPage("all")}>
-                <Folder className="nav-icon"/>
-                <p className="nav-text">All Files</p>
-              </li>
-            </Link>
-            <Link to="/videos">
-              <li className={this.props.page === "videos" ? "active" : " "} onClick={(e) => this.props.setPage("videos")}>
-                <Film className="nav-icon"/>
-                <p className="nav-text">Videos</p>
-              </li>
-            </Link>
-            <Link to="/photos">
-              <li className={this.props.page === "photos" ? "active" : " "} onClick={(e) => this.props.setPage("photos")}>
-                <Image className="nav-icon"/>
-                <p className="nav-text">Photos</p>
-              </li>
-            </Link>
-            <Link to="/upload">
-              <li className={this.props.page === "upload" ? "active" : " "} onClick={(e) => this.props.setPage("upload")}>
-                <CornerRightUp className="nav-icon"/>
-                <p className="nav-text">Upload</p>
-              </li>
-            </Link>
-          </ul>
-        </div>
+        <Settings color="white" size={40} className="nav-icon" />
+      </div>
     );
   }
 }
